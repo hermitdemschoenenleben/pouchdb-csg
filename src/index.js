94169bb619d805de24afd30f87c333ff77685082
Benjamin Wiegand <highwaychile@zoho.com>
2017-06-24 13:54:45 +0200

    use adapter instead of plugin, implement continuous changes feed via websockets

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-import {enableMultipart} from './enable_multipart';
-
-export default function plugin(options={}) {
-  return function(PouchDB) {
-    if (options.multipartProvider) {
-      enableMultipart(PouchDB, options.multipartProvider);
-    }
-  }
-}
-
-/* istanbul ignore next */
-if (typeof window !== 'undefined' && window.PouchDB) {
-  window.PouchDB.plugin(plugin);
-}
+import CSGAdapter from './adapter';
+export default CSGAdapter;
--- src/utils/adapters.js
+++
@@ -1,4 +0,0 @@
-export function updateAdapter(PouchDB, name, transformer) {
-  PouchDB.adapters[name] = transformer(PouchDB.adapters[name])
-  PouchDB.adapters[name].valid = () => true;
-}
